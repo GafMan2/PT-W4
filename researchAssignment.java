0ebb5d4b6e933596b027f31e5c72bbe486fbb7ef
GafMan2 <155397968+GafMan2@users.noreply.github.com>
2024-01-20 15:40:14 -0800

    Add files via upload

---
+++ researchAssignment.java
@@ -0,0 +1,51 @@
+package week04;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import javax.lang.model.util.Elements;
+
+public class researchAssignment {
+
+	public static void main(String[] args) {
+		
+// Week 04 Research Assignment
+		// Choose at least two (2) of the above prompts.
+		// Research those prompts.
+		// Write at least one paragraph for two (2) of the above prompts
+		// Include URLs from where you found the information to cite your sources
+		// Submit your Research Results to the LMS
+		
+// 1. What are the differences between Lists, Sets, and Maps in Java?
+		// For this prompt my source was the Geeks for Geeks website.  I used this site to research explanations for my first chosen prompt which was to describe the difference between Lists, Sets and Maps.
+		
+		// Lists - We use Lists when we want to store ordered collections of data, unlike a Set, Lists can store duplicate values.  A good example of a list would be a list of baseball players and their batting average
+		// https://www.geeksforgeeks.org/list-interface-java-examples/?ref=header_search
+		
+		// Sets - Each item in a set is known as an element and a set cannot include duplicate elements. We use sets when we want to exclude duplicates.  A good example of using a set would be for a shopping cart. You could create a Set object and add each item in a shopping cart to the set. Then, you could use the Set object to search for an item in the shopping cart or to get a list of all the items in the shopping cart.
+		// Sets - https://www.geeksforgeeks.org/internal-working-of-sethashset-in-java/
+		
+		// Maps - A map is an object that maps keys to a specific value. We use maps to perform lookups by keys, a good example would be a map of states and their associated state capitals, 
+		// Maps - https://www.geeksforgeeks.org/map-interface-java-examples/
+
+		
+// 2. Write Java code that does the following:
+		// For this prompt my research source was the Oracle Java Tutorials website and I chose the following prompts where I need to write a line of code to instantiate 3 different types of 
+		// a) Write a line of code that show how you would instantiate an ArrayList of String
+			      ArrayList<String> stringList = new ArrayList<>(); //  This line instantiates an ArrayList of String and the ArrayList can be used to store and manipulate a list of String objects.
+        // a) Array List source: https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/util/ArrayList.html
+			        
+		// b) Write a line of code that show how you would instantiate an HashSet of StringBuilder
+			      HashSet<StringBuilder> stringBuilderSet = new HashSet<>(); // Here I instantiate the HashSet class that can store objects of type StringBuilder.  HashSet can now be used to store and manipulate a collection of StringBuilder objects
+	    //b) StringBuilder source: https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/lang/StringBuilder.html	
+	}
+  {			   
+}
+   		// c) Write a line of code that show how you would instantiate a HashMap of String, String.
+	        HashMap<String, String> stringMap = new HashMap<>(); // Here I instantiate a Hashmap of String by declaring a variable named stringMap with the data type HashMap<String, String>
+	    // c) Hashmap of String souce: https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/util/HashMap.html
+	        {	
+}
+	}
+	    
+
